4c5e625723ad01e7ff611b757fbafbc7f0f79860
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-11-17 17:39:07 +0900

    User MyPage function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user)
   end
 
   def new
@@ -33,7 +33,7 @@ class TweetsController < ApplicationController
   private
 
   def tweet_params
-    params.require(:tweet).permit(:name, :text, :image)
+    params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
   end
 
   def set_tweet
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_064519) do
+ActiveRecord::Schema.define(version: 2020_11_17_083142) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

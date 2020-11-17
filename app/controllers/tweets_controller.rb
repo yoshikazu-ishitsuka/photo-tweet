9915423eda20dda2f849d14f3aa396bb67944c7b
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-11-17 15:00:45 +0900

    Tweet detail function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -17,7 +19,6 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
@@ -25,9 +26,16 @@ class TweetsController < ApplicationController
     tweet.update(tweet_params)
   end
 
+  def show
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:name, :text, :image)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
